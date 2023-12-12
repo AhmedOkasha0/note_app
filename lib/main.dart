f799dd0735c8001695853a088f1332da8fd5d331
non0011 <ao1922302@gamil.com>
2023-12-12 23:02:55 +0200

    init hive and open hive box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:note_app_hive/app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("notesBox");
   runApp(const MyApp());
 }
-
-
