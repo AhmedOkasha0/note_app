599c1b690fc4209ebdbe7d0961d815b6c8ed78f2
non0011 <ao1922302@gamil.com>
2023-12-12 19:58:59 +0200

    add screen util package

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:note_app_hive/core/theme/app_theme.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,9 +7,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: appTheme(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: appTheme(),
+      ),
     );
   }
 }
